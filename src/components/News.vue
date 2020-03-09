fc7e9e4cc8cf73341703b37e619368eb9468135f
tomganchos <tomganchos@mail.ru>
2020-03-09 18:24:43 +0300

    update news block: update info block

--- src/components/News.vue
+++ src/components/News.vue
@@ -17,43 +17,51 @@
       </div>
     </div>
     <div class="container-info">
-      <div>
-        <div class="name-block">
-          <h3>Название новости</h3>
-          <sui-input class="input-name" v-model="name" placeholder="Введите название.." />
+      <div class="container-info__blocks">
+        <div class="block">
+          <div class="label">Название новости</div>
+          <sui-input v-model="title" size="mini"/>
         </div>
-        <div class="date-block">
-          <h3>Дата</h3>
-          <input class="input-date" v-model="date" type="date" placeholder="Введите название..">
+        <div class="block">
+          <div class="label">Дата новости</div>
+          <sui-input v-model="date" type="date" size="mini"/>
         </div>
-        <div class="text-block">
-          <h3>Текст новости</h3>
-          <textarea class="input-text" v-model="text"></textarea>
+        <div class="block">
+          <div class="label">Текст новости</div>
+          <textarea class="input-text" v-model="description"/>
         </div>
-        <div class="link-block">
-          <h3>Ссылки</h3>
-          <div>
-            <sui-button primary>Добавить документ</sui-button>
-            <sui-button negative>Удалить</sui-button>
+
+        <div class="block">
+          <sui-checkbox label="Итоги" v-model="result"/>
+        </div>
+
+        <div class="block">
+          <div class="label">Документы</div>
+          <div class="block-list">
+            <div v-for="link in links" class="block-item">
+              <sui-input class="block-item__title"
+                         placeholder="Название документа"
+                         v-model="link.title" size="mini"/>
+              <sui-input class="block-item__link"
+                         placeholder="Ссылка на документ"
+                         v-model="link.link" size="mini"/>
+            </div>
+            <div class="block-button">
+              <sui-button primary @click="addDoc()">Добавить документ</sui-button>
+              <sui-button v-if="links.length !== 0" negative
+                          @click="removeDoc()">Убрать</sui-button>
+            </div>
           </div>
         </div>
-        <div class="image-block">
-          <h3>Ссылка на изображение</h3>
-          <div>Изображение должно быть горизонтальным</div>
-          <sui-input class="input-image" v-model="img" placeholder="Ссылка.." />
+
+        <div class="block">
+          <div class="label">Изображения</div>
+          <sui-input type="file" multiple v-model="images"/>
         </div>
-        <div class="result-block">
-          <sui-checkbox class="checkbox-result" v-model="result" label="Итоги" />
-          <sui-dropdown
-            placeholder="Отдел"
-            selection
-            :options="listSections"
-            v-model="current"
-          />
-        </div>
-        <div class="buttons">
-          <sui-button primary v-on:click="addNews()">Отправить новость</sui-button>
-        </div>
+      </div>
+      <div class="container-info__buttons">
+        <sui-button negative v-on:click="removeNews()">Удалить новость</sui-button>
+        <sui-button primary v-on:click="addNews()">Сохранить новость</sui-button>
       </div>
     </div>
   </div>
@@ -67,11 +75,12 @@
       data() {
           return {
 
-            name: '',
+            title: '',
             date: '',
-            text: '',
+            description: '',
             docs: [],
-            img: '',
+            links: [],
+            images: null,
             result: false,
             section: this.current,
 
@@ -101,43 +110,55 @@
       methods: {
         addNews: function () {
 
-          if (this.name !== '' && this.date !== '') {
-            axios.post('http://localhost:3012/news', {
-                  name: this.name,
-                  date: this.date + 'T00:00:00.000',
-                  text: this.text,
-                  selection: this.section
-                  }).then((response) => {
-              console.log(response);
-            });
-            axios.post("http://localhost:3012/news", '', {
-              headers: {
-                'Content-Type': 'application/json',
-              },
-              body: JSON.stringify({
-                name: this.name,
-                date: this.date + 'T00:00:00.000',
-                text: this.text,
-                selection: this.section
-                })
-            }).then(function (response) {
-              console.log(response.data);
-              console.log(response.status);
-            }).catch(function (error) {
-              console.log(error.status);
-              if (response.status === 401) {
-                console.log(401);
-              }
-            })
-          } else {
-            console.log('else');
-          }
-          console.log(this.name);
+          // if (this.name !== '' && this.date !== '') {
+          //   axios.post('http://localhost:3012/news', {
+          //         name: this.name,
+          //         date: this.date + 'T00:00:00.000',
+          //     description: description.text,
+          //         selection: this.section
+          //         }).then((response) => {
+          //     console.log(response);
+          //   });
+          //   axios.post("http://localhost:3012/news", '', {
+          //     headers: {
+          //       'Content-Type': 'application/json',
+          //     },
+          //     body: JSON.stringify({
+          //       name: this.name,
+          //       date: this.date + 'T00:00:00.000',
+          //       description: this.description,
+          //       selection: this.section
+          //       })
+          //   }).then(function (response) {
+          //     console.log(response.data);
+          //     console.log(response.status);
+          //   }).catch(function (error) {
+          //     console.log(error.status);
+          //     if (response.status === 401) {
+          //       console.log(401);
+          //     }
+          //   })
+          // } else {
+          //   console.log('else');
+          // }
+          console.log(this.title);
           console.log(this.date);
-          console.log(this.text);
-          console.log(this.img);
+          console.log(this.description);
+          console.log(this.images);
           console.log(this.result);
           console.log(this.current);
+        },
+        removeNews: function() {
+          console.log('remove')
+        },
+        addDoc() {
+          this.links.push({
+            title: '',
+            link: ''
+          })
+        },
+        removeDoc() {
+          this.links.pop()
         },
         getDate(date) {
           return moment(date).locale('ru').format('DD MMMM YYYY')
@@ -147,44 +168,6 @@
 </script>
 
 <style scoped>
-  .vfg-container {
-    max-width: 1200px !important;
-    padding-left: 0 !important;
-    padding-right: 0 !important;
-  }
-  .vfg-grid-item {
-    padding-left: 0 !important;
-    padding-right: 0 !important;
-  }
-
-  .ui.segment {
-    text-align: left;
-    min-width: 300px;
-    max-width: 500px;
-    margin: 5px 5px 200px 5px;
-  }
-  .news-panel {
-    text-align: center;
-    text-align: -moz-center;
-    text-align: -webkit-center;
-  }
-  .input-name, .input-image {
-    width: 470px;
-  }
-  .date-block, .text-block, .link-block, .image-block {
-    margin-top: 10px;
-  }
-  .result-block {
-    margin-top: 15px;
-  }
-  .input-date {
-    border: 1px solid rgba(34,36,38,.15);
-    color: rgba(0,0,0,.87);
-    border-radius: .28571429rem;
-    padding: .67857143em 1em;
-    line-height: 1.21428571em;
-    width: 154px;
-  }
   .input-text {
     border: 1px solid rgba(34,36,38,.15);
     color: rgba(0,0,0,.87);
@@ -192,14 +175,6 @@
     width: 470px;
     height: 150px;
   }
-  .checkbox-result {
-    margin-right: 15px;
-  }
-
-  h3 {
-    margin-bottom: 5px;
-  }
-
   .container {
     height: calc(100vh - 60px);
     display: flex;
@@ -221,6 +196,17 @@
   }
   .container-info {
     width: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+  }
+  .container-info__blocks {
+    overflow: auto;
+  }
+  .container-info__buttons {
+    display: flex;
+    margin: 15px;
+    justify-content: flex-end;
   }
 
   .news {
@@ -236,4 +222,35 @@
   .news-name {
     font-weight: bold;
   }
+
+  .block {
+    margin: 15px;
+  }
+  .block .label {
+    font-weight: bold;
+    margin-bottom: 5px;
+  }
+  .block .input {
+    width: 100%;
+  }
+  .block .input[type="date"] {
+    width: auto;
+    height: 38px;
+  }
+  .block textarea {
+    width: 100%;
+  }
+  .block-item {
+    display: flex;
+    justify-content: space-between;
+    margin-top: 5px;
+    margin-bottom: 15px;
+  }
+  .block-item .block-item__link, .block-item .block-item__title {
+    width: 45%;
+  }
+  .block-button {
+    margin-top: 5px;
+  }
+
 </style>
